b341425d7f073d8322826e3afc0f9c1641253240
Enrique Ortiz <hi@enriqueortiz.dev>
2022-11-22 14:36:28 -0400

    feat: chapter 6 up to 6.11

--- tests/integration.rs
+++ tests/integration.rs
@@ -110,6 +110,35 @@ async fn test_subscribe_returns_200_for_valid_form_data() {
 }
 
 #[tokio::test]
+async fn test_subscribe_returns_400_for_invalid_empty_fields() {
+    let test_app = spawn_app().await;
+    let client = Client::new();
+
+    let test_cases = vec![
+        ("name=&email=paulg@yc.com", "empty name"),
+        ("name=Paul%20Henschel&email=", "empty email"),
+        ("name=Danny&email=not_an_email", "invalid email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", test_app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Could not send request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 when the payload was {}",
+            error_message
+        )
+    }
+}
+
+#[tokio::test]
 async fn test_subscribe_returns_400_for_invalid_form_data() {
     let test_app = spawn_app().await;
     let client = Client::new();
